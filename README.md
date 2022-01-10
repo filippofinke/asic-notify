18e2843c9f5341a017dd15a056f9a100d0f18445
filippofinke <filippofinke@protonmail.ch>
2022-01-10 11:33:06 +0100

    🎉 initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+<h1 align="center">Welcome to asic-notify 👋</h1>
+
+> ⛏️ Get a message on telegram with the profitability of your miner.
+
+### 🏠 [Homepage](https://github.com/filippofinke/asic-notify#readme)
+
+## Install
+
+```sh
+npm install
+```
+
+## Usage
+
+```sh
+BOT_TOKEN=... CHAT_ID=... MINER=... npm run start
+```
+
+## Author
+
+👤 **Filippo Finke**
+
+- Website: https://filippofinke.ch
+- Twitter: [@filippofinke](https://twitter.com/filippofinke)
+- Github: [@filippofinke](https://github.com/filippofinke)
+- LinkedIn: [@filippofinke](https://linkedin.com/in/filippofinke)
+
+## 🤝 Contributing
+
+Contributions, issues and feature requests are welcome!<br />Feel free to check [issues page](https://github.com/filippofinke/asic-notify/issues).
+
+## Show your support
+
+Give a ⭐️ if this project helped you!
+
+## 📝 License
+
+Copyright © 2022 [Filippo Finke](https://github.com/filippofinke).<br />
+This project is [MIT](https://github.com/filippofinke/asic-notify/blob/master/LICENSE) licensed.
+
+<a href="https://www.buymeacoffee.com/filippofinke">
+  <img src="https://github.com/filippofinke/filippofinke/raw/main/images/buymeacoffe.png" alt="Buy Me A McFlurry">
+</a>
---
+++ index.js
@@ -0,0 +1,60 @@
+const fetch = require("node-fetch");
+const { parse } = require("node-html-parser");
+
+const endpoint = "https://www.asicminervalue.com/miners/";
+const { CHAT_ID, BOT_TOKEN, MINER } = process.env;
+
+if (!CHAT_ID || !BOT_TOKEN || !MINER) {
+  console.error("Please set a BOT_TOKEN, CHAT_ID and MINER in your environment!");
+  process.exit(1);
+}
+
+const sendMessage = (message) => {
+  fetch(`https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      chat_id: CHAT_ID,
+      text: message,
+      parse_mode: "Markdown",
+    }),
+  });
+};
+
+fetch(endpoint + MINER)
+  .then((r) => r.text())
+  .then((html) => {
+    const dom = parse(html);
+
+    let title = dom.querySelector("body > div.container > div.row-fluid > h1").innerText.trim();
+
+    let daily = dom
+      .querySelector(
+        "body > div.container > div:nth-child(2) > div:nth-child(2) > table.table.table-striped.rentability > tbody > tr:nth-child(1) > td:nth-child(2)"
+      )
+      .innerText.trim();
+
+    let monthly = dom
+      .querySelector(
+        "body > div.container > div:nth-child(2) > div:nth-child(2) > table.table.table-striped.rentability > tbody > tr:nth-child(1) > td:nth-child(3)"
+      )
+      .innerText.trim();
+
+    let yearly = dom
+      .querySelector(
+        "body > div.container > div:nth-child(2) > div:nth-child(2) > table.table.table-striped.rentability > tbody > tr:nth-child(1) > td:nth-child(4)"
+      )
+      .innerText.trim();
+
+    let today = new Date().toLocaleString();
+
+    let message = `‎\n*${title}*\n\n📈 Daily: *${daily}*\n📈 Monthly: *${monthly}*\n📈 Yearly: *${yearly}*`;
+
+    message = message.replaceAll(",", "'");
+
+    message += `\n‎\n_${today}_`;
+
+    sendMessage(message);
+  });
