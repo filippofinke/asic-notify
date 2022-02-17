35e95add35da20202c3fc52bd1f7e858e6e6adf6
filippofinke <filippofinke@protonmail.ch>
2022-02-17 19:42:14 +0100

    ✨ feat: add electricity costs

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ if (!CHAT_ID || !BOT_TOKEN || !MINER) {
   process.exit(1);
 }
 
+const getFloat = (str) => {
+  return parseFloat(str.replace(/[\$,]/, ""));
+};
+
 const sendMessage = (message) => {
   fetch(`https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`, {
     method: "POST",
@@ -23,60 +27,69 @@ const sendMessage = (message) => {
   });
 };
 
-fetch(endpoint + MINER)
+fetch(endpoint + MINER, {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/x-www-form-urlencoded",
+  },
+  body: "currency=USD&electricitycost=" + PRICE_KVH,
+})
   .then((r) => r.text())
   .then((html) => {
     const dom = parse(html);
 
     let title = dom.querySelector("body > div.container > div.row-fluid > h1").innerText.trim();
 
-    let daily = dom
-      .querySelector(
-        "body > div.container > div:nth-child(2) > div:nth-child(2) > table.table.table-striped.rentability > tbody > tr:nth-child(1) > td:nth-child(2)"
-      )
-      .innerText.trim();
-
-    let monthly = dom
-      .querySelector(
-        "body > div.container > div:nth-child(2) > div:nth-child(2) > table.table.table-striped.rentability > tbody > tr:nth-child(1) > td:nth-child(3)"
-      )
-      .innerText.trim();
-
-    let yearly = dom
-      .querySelector(
-        "body > div.container > div:nth-child(2) > div:nth-child(2) > table.table.table-striped.rentability > tbody > tr:nth-child(1) > td:nth-child(4)"
-      )
-      .innerText.trim();
-
     let today = new Date().toLocaleString();
 
-    let message = `‎\n*${title}*\n\n📈 Daily: *${daily}*\n📈 Monthly: *${monthly}*\n📈 Yearly: *${yearly}*`;
+    let titles = ["Daily", "Monthly", "Yearly"];
+
+    let message = `‎\n*${title}*\n`;
+
+    let income = dom.querySelector(
+      "body > div.container > div:nth-child(2) > div:nth-child(2) > table.table.table-striped.rentability > tbody > tr:nth-child(1)"
+    ).childNodes;
+
+    message += "\n💡 *Income*: ";
+    for (let i = 0; i < 3; i++) {
+      message += `\n📈 ${titles[i]}: *${income[i + 2].innerText.trim()}*`;
+    }
+
+    let electricity = null;
+    if (PRICE_KVH) {
+      electricity = dom.querySelector(
+        "body > div.container > div:nth-child(2) > div:nth-child(2) > table.table.table-striped.rentability > tbody > tr:nth-child(2)"
+      ).childNodes;
+
+      message += "\n\n💡 *Electricity*: ";
+      for (let i = 0; i < 3; i++) {
+        message += `\n📈 ${titles[i]}: *${electricity[i + 2].innerText.trim()}*`;
+      }
+    }
 
     message = message.replaceAll(",", "'");
 
     if (PRICE) {
-      daily = parseFloat(daily.replaceAll("$", "").replaceAll(",", ""));
-      let roi = parseFloat(PRICE) / daily;
+      let daily = getFloat(income[2].innerText);
+
+      if (electricity) {
+        daily += getFloat(electricity[2].innerText);
+      }
+
+      let roi = getFloat(PRICE) / daily;
 
       let months = Math.floor(roi / 30);
       let days = Math.floor(roi - months * 30);
 
-      message += `\n\n💰 ROI: _${months} month`;
-      if (months > 1) {
-        message += "s";
-      }
+      message += `\n\n💰 ROI: _${months} month` + (months > 1 ? "s" : "");
 
       if (days > 0) {
-        message += ` and ${days} day`;
-        if (days > 1) {
-          message += "s";
-        }
+        message += ` and ${days} day` + (days > 1 ? "s" : "");
       }
 
       message += `_`;
     }
 
     message += `\n‎\n_${today}_`;
-
     sendMessage(message);
   });
